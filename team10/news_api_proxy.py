8c48f52fbbfa192f60695f4451c87cfdc91167e8
Paul Whipp <pwhipp@csumb.edu>
2019-05-01 10:41:43 -0700

    Objectifying

--- team10/news_api_proxy.py
+++ team10/news_api_proxy.py
@@ -4,31 +4,25 @@ from newsapi.newsapi_client import NewsApiClient
 
 ###############################
 ### News API Sample Code
+# https://github.com/mattlisiv/newsapi-python/tree/master/newsapi
 
-# https://github.com/mattlisiv/newsapi-python/tree/master/newsapi <-- more info
-
-# Init
-newsapi = NewsApiClient(api_key='00d22965bc8847a692f7689dd668ca3c')
 app = Flask(__name__)
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin', language='en')
-                                          #sources='bbc-news,the-verge',
-                                          #category='business',
-                                          #language='en'
-                                          #country='us')
-#
-# # /v2/everything
-# all_articles = newsapi.get_everything(q='bitcoin',
-#                                       sources='bbc-news,the-verge',
-#                                       domains='bbc.co.uk,techcrunch.com',
-#                                       from_param='2017-12-01',
-#                                       to='2017-12-12',
-#                                       language='en',
-#                                       sort_by='relevancy',
-#                                       page=2)
-#
-# # /v2/sources
-# sources = newsapi.get_sources()
+
+
+class News:
+
+    def __init__(q):
+        newsapi = NewsApiClient(api_key='00d22965bc8847a692f7689dd668ca3c')
+        self.query = q
+
+
+    def get_data():
+        return newsapi.get_top_headlines(q=query, language='en')
+
+
+    def new_query(q):
+        self.query = q
+        return get_data()
 
 
 # default route -- homepage
@@ -38,6 +32,7 @@ def news():
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
 # print(top_headlines)
 # news()
