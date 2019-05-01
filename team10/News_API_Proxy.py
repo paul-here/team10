197140512128bf1475cded8e8bb025e37f759d16
Paul Whipp <pwhipp@csumb.edu>
2019-05-01 11:32:09 -0700

    using main for testing

--- team10/News_API_Proxy.py
+++ team10/News_API_Proxy.py
@@ -7,27 +7,27 @@ from newsapi.newsapi_client import NewsApiClient
 
 class News:
 
-    def __init__(q):
-        newsapi = NewsApiClient(api_key='00d22965bc8847a692f7689dd668ca3c')
+    def __init__(self, q):
+        self.newsapi = NewsApiClient(api_key='00d22965bc8847a692f7689dd668ca3c')
         self.query = q
 
 
-    def get_data():
-        return newsapi.get_top_headlines(q=query, language='en')
+    def get_data(self):
+        return self.newsapi.get_top_headlines(q=self.query, language='en')
 
 
-    def new_query(q):
+    def new_query(self, q):
         self.query = q
         return get_data()
 
 
-# default route -- homepage
-@app.route('/')
-def news():
-    return render_template('newsapi.html', data=news.get_data())
+# # default route -- homepage
+# @app.route('/')
+# def news():
+#     return render_template('newsapi.html', data=news.get_data())
 
 
-if __name__ == "__main__":
-    app = Flask(__name__)
-    news = News('bitcoin')
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app = Flask(__name__)
+#     news = News('bitcoin')
+#     app.run(debug=True)
